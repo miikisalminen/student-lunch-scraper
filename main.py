f55426ff5e65248c70496f22f2c18d8903d8202c
miikisalminen <47534773+miikisalminen@users.noreply.github.com>
2020-08-28 18:11:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import discord
 from scraper import parse
-from dotenv import load_dotenv
 
 
-load_dotenv()
 TOKEN = "BOT TOKEN"
 
 client = discord.Client()
@@ -16,7 +14,6 @@ async def on_message(message):
 		response = parse()
 		print(response)
 		await message.channel.send(response)
-		print("peppu")
 
 async def on_ready():
 	print("ONLINE")
